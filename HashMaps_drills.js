04eb3934d3621106487a26d0c623436debce4470
Jonny Deates <48420377+JonnyDeates@users.noreply.github.com>
2019-12-16 09:47:34 -0700

    Number 1

--- HashMaps_drills.js
+++ HashMaps_drills.js
@@ -1,3 +1,22 @@
 const HashMaps = require('./HashMaps');
 
 //Drill 1: 
+function main() {
+  let lotr = new HashMaps();
+  HashMaps.MAX_LOAD_RATIO = .5;
+  HashMaps.SIZE_RATIO = 3;
+  lotr.set("Hobbit", "Bilbo");
+  lotr.set("Hobbit", "Frodo");
+  lotr.set("Wizard", "Gandolf");
+  lotr.set("Human", "Aragon");
+  lotr.set("Elf", "Legolas");
+  lotr.set("Maiar", "The Necromancer");
+  lotr.set("Maiar", "Sauron");
+  lotr.set("RingBearer", "Gollum");
+  lotr.set("LadyOfLight", "Galadriel");
+  lotr.set("HalfElven", "Arwen");
+  lotr.set("Ent", "Treebeard");
+
+  console.log(lotr.length, lotr.get('Hobbit'), lotr.get('Maiar'), lotr._capacity)
+}
+main();
